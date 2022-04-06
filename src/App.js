f31e87f71f0dd13e12dab608734660867802b03f
PaulaValle <paula.valle1.romero@gmail.com>
2022-04-06 14:02:09 +0200

    added footer and a container

--- src/App.js
+++ src/App.js
@@ -4,9 +4,30 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <footer className="text-center">
+          <a
+            href="https://github.com/PaulaValle/dictionary-project-paula-valle"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Open source
+          </a>
+          coded by{" "}
+          <a
+            href="https://www.linkedin.com/in/paula-valle-romero-a8869a184/"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            {" "}
+            Paula Valle
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
