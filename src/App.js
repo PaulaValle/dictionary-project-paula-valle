2093918a5462fbc9bd37239e9a390f7df18ad4f7
PaulaValle <paula.valle1.romero@gmail.com>
2022-04-06 14:10:09 +0200

    changed style and created dictionary js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from "./paulavallemonograma.png";
 import "./App.css";
+import Dictionary from "./dictionary.js";
 
 export default function App() {
   return (
@@ -8,24 +9,29 @@ export default function App() {
         <header className="App-header">
           <img src={logo} className="App-logo img-fluid" alt="logo" />
         </header>
-        <footer className="text-center">
-          <a
-            href="https://github.com/PaulaValle/dictionary-project-paula-valle"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {" "}
-            Open source
-          </a>
-          coded by{" "}
-          <a
-            href="https://www.linkedin.com/in/paula-valle-romero-a8869a184/"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            {" "}
-            Paula Valle
-          </a>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">
+          <small>
+            <a
+              href="https://github.com/PaulaValle/dictionary-project-paula-valle"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              Open source
+            </a>
+            coded by{" "}
+            <a
+              href="https://www.linkedin.com/in/paula-valle-romero-a8869a184/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              {" "}
+              Paula Valle
+            </a>
+          </small>
         </footer>
       </div>
     </div>
---
+++ src/dictionary.js
@@ -0,0 +1,6 @@
+import React from "react";
+import "./dictionary.css";
+
+export default function dictionary() {
+  return <div className="dictionary">Hello from dictionary</div>;
+}
